62c794772d95118078905aeb8565bb6886706f1c
JoeyUCF <jo970060@ucf.edu>
2024-02-15 17:03:34 -0500

    Initial Commit

--- lab_assignment_5.c
+++ lab_assignment_5.c
@@ -9,15 +9,15 @@ typedef struct node {
 // Returns number of nodes in the linkedList.
 int length(node* head)
 {
-   struct node *tmp = head;
-    int len = 0;
-   while (tmp != NULL)
-   {
-      tmp = tmp->next;
-      len++;
-   }
+	int count = 0;
+	node* current = head;
 
-   return (len);
+	// Iterates through the list sand increments for every node
+	while(current != NULL){
+		count++;
+		current = current->next;
+	}
+	return count;
 }
 
 // parses the string in the linkedList
@@ -25,6 +25,28 @@ int length(node* head)
 //  then toCString function wil return "abc"
 char* toCString(node* head)
 {
+	int len = length(head);
+	char* str = (char*)malloc((len + 1) * sizeof(char)); // Additional one is to account for null terminator
+
+	// Checks if the Str was allocated
+	if (str == NULL){
+		printf("Memory did not allocate");
+		return NULL;
+	}
+
+	int i = 0;
+	node* current = head;
+
+	// Iterates through the nodes and sets the str[index] to the current letter
+	while(current != NULL){
+		str[i] = current->letter;
+		current = current->next;
+		i++;
+	}
+
+	// Null operator at the end of the string
+	str[i] = '\0';
+	return str;
 }
 
 // inserts character to the linkedlist
@@ -33,11 +55,43 @@ char* toCString(node* head)
 // head -> |a|->|b|->|c|->|x|
 void insertChar(node** pHead, char c)
 {
+
+	// Creates new node in memory and checks if it was allocated
+	node* newNode = (node*)malloc(sizeof(node));
+	if (newNode == NULL){
+		return;
+	}
+
+	// Initializes newNode members
+	newNode->letter = c;
+	newNode->next = NULL;
+
+	// Checks if the provided pNode is NULL
+	if(*pHead == NULL){
+		*pHead = newNode;
+	} else { // Iterates through the list while it is not null
+		node* current = *pHead;
+		while(current->next != NULL){
+			current = current->next;
+		}
+		// After finding a null head (end of list) it points the last node to the new node.
+		current->next = newNode;
+	}
 }
 
 // deletes all nodes in the linkedList.
 void deleteList(node** pHead)
 {
+	node* current = *pHead;
+	node* next;
+
+	while (current != NULL){ // While NOT NULL
+		next = current->next; // Stores the pointer to the next node in NEXT
+		free(current); // Frees the current NODE
+		current = next; // Sets current NODE to the next pointer to continue iterating
+	}
+	*pHead = NULL; // Sets the head of the list to NULL after deleting everything
+
 }
 
 int main(void)
